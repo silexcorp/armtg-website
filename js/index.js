cc8a5ec53aae4824ef6f24f6444d76f594c4709a
Cody Bennett <codyb2129@gmail.com>
2019-01-16 18:49:10 -0600

    Updated interface to VR design.
    Accounts, interactions, and interfaces' functionality are still in progress.

--- js/index.js
+++ js/index.js
@@ -1,18 +1,50 @@
-var scrolled;
-$(window).scroll(() => {
-    let scroll = $(window).scrollTop();
-    if (scroll > 0) {
-        $('header').addClass('scrolled');
-		$('section').addClass('active');
-		document.body.style.height = "100%";
-		document.getElementById('scroll').remove();
-		scrolled = true;
-    } else if (scroll == 0 && !scrolled) {
-        $('header').removeClass('scrolled');
+var SELECTORS = {
+  section: '[data-section]',
+  scrollTo: '[data-scroll-to]',
+  scrollDir: '[data-scroll-dir]' };
+
+var sectionsArray = Array.from(document.querySelectorAll(SELECTORS.section));
+var scrollToElements = document.querySelectorAll(SELECTORS.scrollTo);
+var scrollDirElements = document.querySelectorAll(SELECTORS.scrollDir);
+
+var currentSectionIndex = 0;
+
+var getScrollTarget = function getScrollTarget(dir) {
+  if (dir === 'prev' && currentSectionIndex > 0) {
+    currentSectionIndex--;
+    return sectionsArray[currentSectionIndex];
+  }
+  if (dir === 'next' && currentSectionIndex < sectionsArray.length - 1) {
+    currentSectionIndex++;
+    return sectionsArray[currentSectionIndex];
+  }
+  return false;
+};
+
+scrollDirElements.forEach(function (el) {
+  el.addEventListener('click', function () {
+    var direction = el.dataset.scrollDir;
+    var target = getScrollTarget(direction);
+
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
     }
+  });
 });
 
-$('#scroll').click(function(e){
-  var $target = $('html,body');
-  $target.animate({scrollTop: $target.height()}, 1);
+scrollToElements.forEach(function (el) {
+  el.addEventListener('click', function (e) {
+    e.preventDefault();
+    var targetId = el.getAttribute('href');
+    var target = document.querySelector(targetId);
+
+    if (target) {
+      sectionsArray.forEach(function (section, index) {
+        if (section.id === targetId.replace('#', '')) {
+          currentSectionIndex = index;
+        }
+      });
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
 });
