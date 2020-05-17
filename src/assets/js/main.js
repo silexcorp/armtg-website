eecb852136f4cf2b474e993bd7328e3bb9f63bdf
Cody Bennett <hi@codyb.co>
2020-05-17 11:54:52 -0500

    Updated sitemap.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -19,64 +19,61 @@ class ShapeOverlays {
 	toggle() {
 		this.isAnimating = true;
 		const range = Math.random() * Math.PI * 2;
-		for (var i = 0; i < this.numPoints; i++) {
+		for (let i = 0; i < this.numPoints; i++) {
 			const radian = (i / (this.numPoints - 1)) * Math.PI * 2;
 			this.delayPointsArray[i] = (Math.sin(radian + range) + 1) / 2 * this.delayPointsMax;
 		}
-		if (this.isOpened === false) {
-			this.open();
-		} else {
-			this.close();
-		}
+
+		return this.isOpened === false ? this.open() : this.close();
 	}
 	open() {
 		this.isOpened = true;
 		this.elm.classList.add('is-opened');
 		this.timeStart = Date.now();
+
 		this.renderLoop();
 	}
 	close() {
 		this.isOpened = false;
 		this.elm.classList.remove('is-opened');
 		this.timeStart = Date.now();
+
 		this.renderLoop();
 	}
 	updatePath(time) {
 		const points = [];
-		for (var i = 0; i < this.numPoints; i++) {
+		for (let i = 0; i < this.numPoints; i++) {
 			points[i] = (1-ease.cubicInOut(Math.min(Math.max(time - this.delayPointsArray[i], 0) / this.duration, 1))) * 100
 		}
 
 		let str = '';
 		str += (this.isOpened) ? `M 0 0 H ${points[0]}` : `M ${points[0]} 0`;
-		for (var i = 0; i < this.numPoints - 1; i++) {
+		for (let i = 0; i < this.numPoints - 1; i++) {
 			const p = (i + 1) / (this.numPoints - 1) * 100;
 			const cp = p - (1 / (this.numPoints - 1) * 100) / 2;
 			str += `C ${points[i]} ${cp} ${points[i + 1]} ${cp} ${points[i + 1]} ${p} `;
 		}
+
 		str += (this.isOpened) ? `H 100 V 0` : `H 0 V 0`;
 		return str;
-
 	}
 	render() {
 		if (this.isOpened) {
-			for (var i = 0; i < this.path.length; i++) {
+			for (let i = 0; i < this.path.length; i++) {
 				this.path[i].setAttribute('d', this.updatePath(Date.now() - (this.timeStart + this.delayPerPath * i)));
 			}
 		} else {
-			for (var i = 0; i < this.path.length; i++) {
+			for (let i = 0; i < this.path.length; i++) {
 				this.path[i].setAttribute('d', this.updatePath(Date.now() - (this.timeStart + this.delayPerPath * (this.path.length - i - 1))));
 			}
 		}
 	}
 	renderLoop() {
 		this.render();
+
 		if (Date.now() - this.timeStart < this.duration + this.delayPerPath * (this.path.length - 1) + this.delayPointsMax) {
-			requestAnimationFrame(() => {
-				this.renderLoop();
-			});
-		}
-		else {
+			requestAnimationFrame(() => this.renderLoop());
+		} else {
 			this.isAnimating = false;
 		}
 	}
